e0822b07f680a5ea42a9a56c0c20272e60b25bee
Alex Simisi Nta <smsxander@gmail.com>
2022-04-03 21:01:17 +0200

    init project with NextJS, adding tailwind css and creation of Header component

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: 'jit', // Just in time
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
